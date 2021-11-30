92f8e15a30e96f30ec0fea9c23cebfbe6a570513
parsajiravand <parsajiravand@gmail.com>
2021-12-01 02:43:26 +0330

    setup typeORM

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,9 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+/* you can use decrators togeter For GraphQL And TypeOrm(DATABASE) */
 @ObjectType()
+@Entity()
 export class Restaurant {
-  @Field((type) => String)
+  @PrimaryGeneratedColumn()
+  @Field(() => Number)
+  id: number;
+
+  @Field(() => String)
+  @Column()
   name: string;
+
   @Field(() => Boolean, { nullable: true })
-  isFree?: boolean;
+  @Column()
+  isVegan: boolean;
+
+  @Field(() => String)
+  @Column()
+  address: string;
+
+  @Field(() => String)
+  @Column()
+  ownersName: string;
+
+  @Field(() => String)
+  @Column()
+  category: string;
 }
